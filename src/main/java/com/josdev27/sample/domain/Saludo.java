f2562d3f3f4d9a205d171a72701bd1a24d0e8b10
Jos Galiano <josdev27@gmail.com>
2019-11-25 13:12:35 +0100

    Bug corrected

--- src/main/java/com/josdev27/sample/domain/Saludo.java
+++ src/main/java/com/josdev27/sample/domain/Saludo.java
@@ -6,6 +6,7 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
@@ -18,6 +19,7 @@ public class Saludo implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @Id
     private int id;
  
     @Column(name = "saludo")
