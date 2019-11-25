c4ac857dc5013c332185a534cc72e4a231ecdaff
Jos Galiano <josdev27@gmail.com>
2019-11-25 11:59:25 +0100

    Cross added

--- src/main/java/com/josdev27/sample/controller/DemoController.java
+++ src/main/java/com/josdev27/sample/controller/DemoController.java
@@ -3,6 +3,7 @@ package com.josdev27.sample.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,6 +14,7 @@ import com.josdev27.sample.service.DemoService;
 
 @RestController
 @RequestMapping(value = "/demo", produces = MediaType.APPLICATION_JSON_VALUE)
+@CrossOrigin
 public class DemoController {
 
     @Autowired
